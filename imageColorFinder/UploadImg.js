750a334cd793727869b2df66221afec33e1f574d
surajbca <krsurajkumar7481@gmail.com>
2023-06-16 20:15:43 +0530

    setup new project

--- imageColorFinder/UploadImg.js
+++ imageColorFinder/UploadImg.js
@@ -2,7 +2,13 @@ function preview_image(event) {
   let colorDiv = document.getElementById("colorDiv");
   colorDiv.style.display = "";
 
-  let pic = document.getElementById("pic");
+  let imgdiv = document.getElementById("imgdiv");
+  imgdiv.style.display = "";
+
+  let image_before = document.getElementById("image_before");
+  image_before.style.display = "none";
+
+  let pic = document.getElementById("filename");
   pic.style.display = "";
   var reader = new FileReader();
   reader.onload = function () {
@@ -10,4 +16,6 @@ function preview_image(event) {
     output.src = reader.result;
   };
   reader.readAsDataURL(event.target.files[0]);
+  console.log("image name : " + event.target.files[0].name);
+  pic.innerHTML = event.target.files[0].name;
 }
